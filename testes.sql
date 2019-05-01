e1f0b6e4a7a28b1987020db6f1e599aef833c02f
unknown <joao.macarrony@gmail.com>
2019-05-01 21:18:20 +0100

    Added testes.sql

---
+++ testes.sql
@@ -0,0 +1,12 @@
+USE SI2;
+
+/* Testes */
+
+exec ForceDeleteProduto 4
+
+select * from Stock
+select * from Produto
+
+exec InsertStock 4,4,4,4,4,2;
+
+exec InsertProduto 5,'Alimentar','ola',5,5,5,1
