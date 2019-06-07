732a0560134ba546826d6f4152f97007a4a41267
João Dias <joao.macarrony@gmail.com>
2019-06-07 19:42:07 +0100

    Added CO Project

---
+++ Project/SI2_CO/Entities/Franqueado.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+
+namespace SI2_CO.Entities
+{
+    [Table(Name = "Franqueado")]
+    public class Franqueado
+    {
+    }
+}
---
+++ Project/SI2_CO/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using System.Data.SqlClient;
+
+namespace SI2_CO
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnectionStringBuilder csb = new SqlConnectionStringBuilder();
+            csb.DataSource = "localhost";
+            csb.InitialCatalog = "SI2"; 
+            csb.UserID = "sa";
+            csb.Password = "1234";
+
+            using (SqlConnection cn = new SqlConnection(csb.ConnectionString))
+            {
+                string strSQL = "UPDATE Franqueado SET morada = 'Av. do Brasil Nº 7' WHERE fid = 1";
+                cn.Open();
+                SqlCommand cmd = new SqlCommand(strSQL, cn);
+                Console.WriteLine(cmd.ExecuteNonQuery() + " rows affected");
+                cn.Close();
+            }
+        }
+    }
+}
