649826d989d9a7d8f232365ffbfd1e174dc71932
Ricardo Bogas <19ricardo.bogas97@gmail.com>
2019-06-07 21:40:55 +0100

    Updated project structure and started Fornecedor table -> Connected Objects

---
+++ Project/SI2_CO/Entities/Fornecedor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+
+namespace SI2_CO.Entities
+{
+    [Table(Name = "Fornecedor")]
+    public class Fornecedor
+    {
+        public int Foid { get; set; }
+        public int Nif { get; set; }
+        public string Nome { get; set; }
+    }
+}
